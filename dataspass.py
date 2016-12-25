76760b544388fb4b5c4a0a2c510e21160e49ced9
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2016-12-25 16:53:21 +0100

    Fix formatting and result handling

--- dataspass.py
+++ dataspass.py
@@ -6,23 +6,20 @@ from urllib.request import urlopen
 
 class DataSpass(HTMLParser):
     __matched = False
-    result = list()
+    __result = list()
 
     def handle_starttag(self, tag, attrs):
         if tag == 'div' and [
-            a for a in attrs if 'class' in a[0] and
-                'barTextBelow' in a[-1]
+                a for a in attrs if 'class' in a[0] and 'barTextBelow' in a[-1]
         ]:
             self.__matched = True
         if tag == 'td' and [
-            a for a in attrs if 'class' in a[0] and
-                any([
+                a for a in attrs if 'class' in a[0] and any([
                     all([
                         b in a[-1] for b in [c, 'infoValue']
                     ])
                 ] for c in ['expiryTime', 'remainingTime'])
         ]:
-            self.result.append(' ')
             self.__matched = True
 
     def handle_endtag(self, tag):
@@ -31,16 +28,23 @@ class DataSpass(HTMLParser):
 
     def handle_data(self, data):
         if self.__matched:
-            self.result.append(data)
+            self.__result.append(''.join([data.strip(), ' ']))
+
+    def error(self, message):
+        pass
+
+    @property
+    def result(self):
+        return ''.join(self.__result).strip()
 
 
 def main():
     parser = DataSpass()
 
-    with urlopen('http://datapass.de/home?continue=true') as s:
-        html = s.read().decode('utf-8')
+    with urlopen('http://datapass.de/home?continue=true') as resp:
+        html = resp.read().decode('utf-8')
         parser.feed(html)
-    return ''.join(parser.result).strip()
+    return parser.result
 
 
 if __name__ == '__main__':
