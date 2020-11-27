3082c8306d7f4e86fbe4e39bb2e2e60815237420
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2020-11-27 18:34:13 +0100

    Adjust script to current website redesign

--- dataspass.py
+++ dataspass.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from html.parser import HTMLParser
+from ssl import PROTOCOL_SSLv23, SSLContext
+from sys import exit as _exit
 from urllib.error import URLError
 from urllib.parse import urlencode
 from urllib.request import urlopen
@@ -8,43 +10,44 @@ from urllib.request import urlopen
 
 class DataSpass(HTMLParser):
     __matched = False
-    __result = list()
+    __result = []
 
     def handle_starttag(self, tag, attrs):
-        if tag in ('div', 'td'):
+        if tag == 'div':
             for name, values in attrs:
-                if name == 'class' and any([
+                if name == 'class' and all([
                         val in values for val in
-                        ('barTextBelow', 'remainingTime', 'expiryTime')
+                        ('volume', 'fit-text-to-container')
                 ]):
                     self.__matched = True
 
     def handle_endtag(self, tag):
-        if self.__matched and tag in ('div', 'td'):
+        if self.__matched and tag == 'div':
             self.__matched = False
 
     def handle_data(self, data):
         if self.__matched:
-            self.__result.append(data.strip())
+            self.__result.append(data.strip().rstrip('/'))
 
     def error(self, message):
         pass
 
     @property
     def result(self):
-        return ' '.join(self.__result).strip()
+        return 'left: {}'.format(' of '.join(self.__result)).strip()
 
 
 def main():
     parser = DataSpass()
+    context = SSLContext(PROTOCOL_SSLv23)
     address = '?'.join([
-        'http://datapass.de/home', urlencode({
-            'continue': 'true', 'lang': 'en'
+        'https://datapass.de/home', urlencode({
+            'continue': 'true',
         })
     ])
 
     try:
-        with urlopen(address) as resp:
+        with urlopen(address, context=context) as resp:
             html = resp.read().decode('utf-8')
             parser.feed(html)
     except URLError:
@@ -55,4 +58,4 @@ def main():
 
 
 if __name__ == '__main__':
-    exit(not main())
+    _exit(not main())
